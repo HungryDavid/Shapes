ffcf53e9a9da840725441a39a99e95824f58cfe1
HungryDavid <obardavidangelo@gmail.com>
2025-08-12 18:16:04 +0800

    Added square, rectangle and triangle Functions

---
+++ OneDrive/Desktop/PROJECTS/C PROJECTS/Shapes/main.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include "Functions.c"
+
+int main(){
+  int row, col;
+  getShape(&row, &col);
+  return 0;
+}
---
+++ OneDrive/Desktop/PROJECTS/C PROJECTS/Shapes/main.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+void getShape(int *row, int *col);
+void makeSquare(int n);
+void makeRectangle(int row, int col);
+void makeTriangle(int n);
